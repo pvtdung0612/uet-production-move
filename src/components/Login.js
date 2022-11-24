cc19ad8c6ada63a06291cb92ec69eae4a26b90bc
pvtdung0612 <108540740+pvtdung0612@users.noreply.github.com>
2022-11-24 20:07:22 +0700

    Create screen login

---
+++ src/components/Login.js
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBIcon,
+} from "mdb-react-ui-kit";
+import "./Login.css";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      isShowPassword: false,
+    };
+  }
+
+  handleOnChangeUsername = (event) => {
+    // console.log(event.target.value);
+    this.setState({
+      username: event.target.value,
+    });
+  };
+
+  handleOnChangePassword = (event) => {
+    // console.log(event.target.value);
+    this.setState({
+      password: event.target.value,
+    });
+  };
+
+  handleLogin = () => {
+    alert("btn");
+  };
+
+  handleShowHidePassword = () => {
+    this.setState({
+      isShowPassword: !this.state.isShowPassword,
+    });
+  };
+
+  render() {
+    return (
+      <MDBContainer fluid>
+        <MDBRow className="login-card d-flex justify-content-center align-items-center h-100">
+          <MDBCol col="12">
+            <MDBCard
+              className="bg-dark text-white my-5 mx-auto"
+              style={{ borderRadius: "1rem", maxWidth: "400px" }}
+            >
+              <MDBCardBody className="p-5 d-flex flex-column align-items-center mx-auto w-100">
+                <h2 className="fw-bold mb-4 text-uppercase">Login</h2>
+
+                <div className="panel-input">
+                  <MDBInput
+                    wrapperClass="mb-4"
+                    labelClass="text-white"
+                    label="Username"
+                    id="formControlLg"
+                    type="text"
+                    size="lg"
+                    onChange={(event) => this.handleOnChangeUsername(event)}
+                  />
+                  <div className="input-password">
+                    <MDBInput
+                      wrapperClass="mb-4"
+                      labelClass="text-white"
+                      label="Password"
+                      id="formControlLg"
+                      type={this.state.isShowPassword ? "text" : "password"}
+                      size="lg"
+                      onChange={(event) => this.handleOnChangePassword(event)}
+                    />
+                    <span
+                      className="image-password-eye"
+                      onClick={() => this.handleShowHidePassword()}
+                    >
+                      <i
+                        class={
+                          this.state.isShowPassword
+                            ? "fa-solid fa-eye"
+                            : "fa-sharp fa-solid fa-eye-slash"
+                        }
+                      ></i>
+                    </span>
+                  </div>
+                </div>
+                <MDBBtn
+                  outline
+                  className="mx-2 px-5"
+                  size="lg"
+                  onClick={() => this.handleLogin()}
+                >
+                  Login
+                </MDBBtn>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    );
+  }
+}
+
+export default Login; // Don’t forget to use export default
---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Login from "../components/Login";
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <Login></Login>
+      </div>
+    );
+  }
+}
+
+export default App; // Don’t forget to use export default!
